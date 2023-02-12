3dce9f09f4e04562a2ba72aa7f26ccc4f1135761
ABabayan32 <arpinbabayan90@gmail.com>
2023-02-12 18:59:38 +0100

    Login Page with router

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+
+<script>
+export default {
+  data(){
+    return {
+      login: '',
+      password: ''
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <div>
+    <p>Log in to proceed</p>
+    <input v-model="login" placeholder="login" />
+    <input type="password" v-model="password" placeholder="password" />
+    <button @click="">Login</button>
+  </div>
+</template>
